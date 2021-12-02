3c2f10806fad7ff0aca5ead11f8a6e6294a86ecf
waffenssrush <52982493+waffenssrush@users.noreply.github.com>
2021-12-02 11:59:48 +0800

    Add files via upload

---
+++ leetcode1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace leetcode1
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int k = i + 1; k < nums.Length; k++)
+                {
+                    if (nums[i] + nums[k] == target)
+                    {
+                        return new int[] { i, k };
+                    }
+                }
+            }
+            return new int[0];
+        }
+    }
+}
