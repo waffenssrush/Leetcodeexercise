4e3daa8ffebacd705c3e4627f42628f27f804571
waffenssrush <52982493+waffenssrush@users.noreply.github.com>
2021-12-03 09:31:48 +0800

    Add files via upload

---
+++ leetcode9.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace leetcode9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int rev = 0;
+            int reminder = 0;
+            int x1 = x;
+            for (int i = 0; x1 > 0; i++)
+            {
+                reminder = x1 % 10;
+                x1 = x1 / 10;
+                rev = rev * 10 + reminder;
+            }
+            if (rev == x)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
